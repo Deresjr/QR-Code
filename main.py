f81610c79dc4d21450a05c2be120af52fc3dc7dc
Deres Benn <107571362+Deresjr@users.noreply.github.com>
2023-04-07 19:10:23 +0000

    Typos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # define the functions needed: add, sub, mul, div
 # print options to the user 
-# asl for values
+# ask for values
 # call the functions
 # while loop to continue the program until the user wants to exit
 
